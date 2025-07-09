873b13fa85552e63e48fe031754f781383d852c1
Vedanshimishra <143635091+Vedanshimishra@users.noreply.github.com>
2025-07-09 09:28:39 +0530

    Update Music_Genre_App.py

--- Music_Genre_App.py
+++ Music_Genre_App.py
@@ -90,21 +90,7 @@ if(app_mode=="Home"):
     st.markdown("""
 **Our goal is to help in identifying music genres from audio tracks efficiently. Upload an audio file, and our system will analyze it to detect its genre. Discover the power of AI in music analysis!**
 
-### How It Works
-1. **Upload Audio:** Go to the **Genre Classification** page and upload an audio file.
-2. **Analysis:** Our system will process the audio using advanced algorithms to classify it into one of the predefined genres.
-3. **Results:** View the predicted genre along with related information.
 
-### Why Choose Us?
-- **Accuracy:** Our system leverages state-of-the-art deep learning models for accurate genre prediction.
-- **User-Friendly:** Simple and intuitive interface for a smooth user experience.
-- **Fast and Efficient:** Get results quickly, enabling faster music categorization and exploration.
-
-### Get Started
-Click on the **Genre Classification** page in the sidebar to upload an audio file and explore the magic of our Music Genre Classification System!
-
-### About Us
-Learn more about the project, our team, and our mission on the **About** page.
 """)
 
 
